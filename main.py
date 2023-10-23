743d86c62dca3c30d3c0b9505f153f49c94e6745
violetqed <epiphanott@gmail.com>
2023-10-22 23:03:23 -0700

    added get_most_common_word to report

--- main.py
+++ main.py
@@ -10,20 +10,43 @@ def get_character_dict(text):
             character_dict[char] += 1
     return character_dict
 
+def get_word_dict(text):
+    word_dict = {}
+    words = text.lower().split()
+    for word in words:
+        if word not in word_dict:
+            word_dict[word] = 0
+        word_dict[word] += 1
+    return word_dict
+
+def get_most_common_word(text):
+    most_common = ""
+    highest_count = 0
+    word_dict = get_word_dict(text)
+
+    for word in word_dict:
+        if word_dict[word] > highest_count:
+            most_common = word
+            highest_count = word_dict[word]
+    return (most_common, highest_count)
+
+
 def report(text, path):
-    reportstring = f"--- Begin report of {path} ---\n"
+    report_string = f"--- Begin report of {path} ---\n"
     
     num_words = get_num_words(text)
-    reportstring += f"{num_words} words found in the document\n\n"
+    report_string += f"{num_words} words found in the document\n"
+    most_common = get_most_common_word(text)
+    report_string += f"{most_common[0]} was the most common word found in the document, found {most_common[1]} times\n\n"
 
     character_dict = get_character_dict(text)
     characters = sorted(list(character_dict.keys()))
     for char in characters:
-        reportstring += f"The '{char}' character was found {character_dict[char]} times\n"
+        report_string += f"The '{char}' character was found {character_dict[char]} times\n"
     
-    reportstring += "--- End report---"
+    report_string += "--- End report ---"
 
-    return reportstring
+    return report_string
 
 path = "books/frankenstein.txt"
 
