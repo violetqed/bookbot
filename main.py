c8532c18dd16f4fb730c93f3f6d22134f6c42872
violetqed <epiphanott@gmail.com>
2023-10-22 23:19:14 -0700

    added percent most common word is of words

--- main.py
+++ main.py
@@ -20,15 +20,15 @@ def get_word_dict(text):
     return word_dict
 
 def get_most_common_word(text):
-    most_common = ""
+    most_common_word = ""
     highest_count = 0
     word_dict = get_word_dict(text)
 
     for word in word_dict:
         if word_dict[word] > highest_count:
-            most_common = word
+            most_common_word = word
             highest_count = word_dict[word]
-    return (most_common, highest_count)
+    return (most_common_word, highest_count)
 
 
 def report(text, path):
@@ -36,8 +36,12 @@ def report(text, path):
     
     num_words = get_num_words(text)
     report_string += f"{num_words} words found in the document\n"
+
     most_common = get_most_common_word(text)
-    report_string += f"{most_common[0]} was the most common word found in the document, found {most_common[1]} times\n\n"
+    most_common_word = most_common[0]
+    most_common_word_count = most_common[1]
+    most_common_word_percent = ((most_common_word_count / num_words) * 100)
+    report_string += f"\"{most_common_word}\" is the most common word in the document ({most_common_word_count} times), representing {most_common_word_percent:.2f}% of words.\n\n"
 
     character_dict = get_character_dict(text)
     characters = sorted(list(character_dict.keys()))
@@ -45,11 +49,11 @@ def report(text, path):
         report_string += f"The '{char}' character was found {character_dict[char]} times\n"
     
     report_string += "--- End report ---"
-
     return report_string
 
-path = "books/frankenstein.txt"
 
+
+path = "books/frankenstein.txt"
 with open(path) as f:
     text = f.read()
     print(report(text, path))
